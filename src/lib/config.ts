4f6ac01480ed366b3392a631511bb3bcb9bfab0f
joelee7928 <153342393+joelee7928@users.noreply.github.com>
2025-09-27 11:05:54 +0700

    Add files via upload

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, no-console, @typescript-eslint/no-non-null-assertion */
 
+import { getStorage } from '@/lib/db';
+
 import { AdminConfig } from './admin.types';
-import { getStorage } from './db';
 import runtimeConfig from './runtime';
 
 export interface ApiSite {
